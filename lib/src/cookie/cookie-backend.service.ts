d87c6d481784e1fc8e1a1da10e4483fa3dc61fb0
Кравченко Игорь <iak28@europlan.ru>
2019-04-26 11:52:40 +0300

    fix: override put function instead of trying to set cookieString https://github.com/salemdar/ngx-cookie/pull/82

--- lib/src/cookie/cookie-backend.service.ts
+++ lib/src/cookie/cookie-backend.service.ts
@@ -3,13 +3,15 @@ import { REQUEST, RESPONSE } from '@nguniversal/express-engine/tokens';
 
 import { CookieService } from './cookie.service';
 import { CookieOptionsProvider } from './cookie-options-provider';
+import { CookieOptions } from './cookie-options.model';
 
 @Injectable()
 export class CookieBackendService extends CookieService {
-
-  constructor(@Inject(REQUEST) private request: any,
-              @Inject(RESPONSE) private response: any,
-              _optionsProvider: CookieOptionsProvider) {
+  constructor(
+    @Inject(REQUEST) private request: any,
+    @Inject(RESPONSE) private response: any,
+    _optionsProvider: CookieOptionsProvider,
+  ) {
     super(_optionsProvider);
   }
 
@@ -17,8 +19,14 @@ export class CookieBackendService extends CookieService {
     return this.request.cookie || this.request.headers['cookie'] || '';
   }
 
-  protected set cookieString(val: string) {
-    this.request.cookie = val;
-    this.response.cookie = val;
+  put(key: string, value: string, options: CookieOptions = {}) {
+    this.getAll()[key] = value;
+    this.request.headers.cookie = Object.keys(this.getAll())
+      .map((key) => {
+        return `${key}=${this.get(key)}`;
+      })
+      .join('; ');
+
+    this.response.cookie(key, value, options);
   }
 }
